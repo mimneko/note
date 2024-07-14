8e0a4beafeca633ea95e85131c13b5cd96bed52f
みむねこ <153196561+mimneko@users.noreply.github.com>
2024-07-14 12:55:44 +0900

    Create auto_title.rb

---
+++ _plugins/auto_title.rb
@@ -0,0 +1,18 @@
+require 'jekyll'
+
+module Jekyll
+  class AutoTitleGenerator < Generator
+    def generate(site)
+      site.pages.each do |page|
+        unless page.data['title']
+          if page.content =~ /^#\s+(.+)$/
+            title = page.content.match(/^#\s+(.+)$/)[1]
+            page.data['title'] = title
+          else
+            page.data['title'] = '無題'
+          end
+        end
+      end
+    end
+  end
+end
